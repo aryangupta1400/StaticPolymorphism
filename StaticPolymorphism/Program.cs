ca8567e2d14a1e0895bacb869f24390d00b77874
Aryan Gupta <Aryan.Gupta@oneadvanced.com>
2022-10-30 18:19:01 +0530

    modified

--- StaticPolymorphism/Program.cs
+++ StaticPolymorphism/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+// 2) Print Firstname, Lastname, City, Mobile number, Email using Static polimorphism
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,35 @@ namespace StaticPolymorphism
     {
         static void Main(string[] args)
         {
+            Employee employee = new Employee("Aryan", "Gupta", "Surat, Gujarat", 1234567890, "aryan.gupta@oneadvanced.com");
+
+            employee.display();
+            Console.ReadKey();
         }
     }
+
+    class Employee
+    {
+        string firstName, lastName, city, email;
+        int mobileNumber;
+
+        public Employee(string firstName, string lastName, string city, int mobileNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.city = city;
+            this.mobileNumber = mobileNumber;
+            this.email = email; 
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+            Console.WriteLine("Home City: " + city);
+            Console.WriteLine("Contact Number: " + mobileNumber);
+            Console.WriteLine("Work Email: " + email);
+        }
+
+    }
+
 }
